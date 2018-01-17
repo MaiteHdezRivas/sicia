cd07b8e48882d0719e1d3b64cc258da1ea90f6a2
MaiteHdezRivas <maite.hernandez@gmail.com>
2018-01-17 09:18:52 +0100

    remove tables at schema

--- db/schema.rb
+++ db/schema.rb
@@ -168,16 +168,6 @@ ActiveRecord::Schema.define(version: 20180108095043) do
     t.index ["trading_year"], name: "index_requeriments_on_trading_year", using: :btree
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.string   "resource_type"
-    t.integer  "resource_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
-    t.index ["name"], name: "index_roles_on_name", using: :btree
-  end
-
   create_table "sap_codes", force: :cascade do |t|
     t.string   "sap_field"
     t.string   "sicia_att"
@@ -231,12 +221,6 @@ ActiveRecord::Schema.define(version: 20180108095043) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  end
-
-  create_table "users_roles", id: false, force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "role_id"
-    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
   end
 
   add_foreign_key "ic_action_requeriments", "ic_actions"
